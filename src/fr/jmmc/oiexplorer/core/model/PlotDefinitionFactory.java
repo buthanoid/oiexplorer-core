e8a8fe28b9078e491abdbdab44569861a9455e38
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2012-08-29 09:53:41 +0000

    use new PlotDefinition objects and load presets from file

--- src/fr/jmmc/oiexplorer/core/model/PlotDefinitionFactory.java
+++ src/fr/jmmc/oiexplorer/core/model/PlotDefinitionFactory.java
@@ -3,12 +3,24 @@
  ******************************************************************************/
 package fr.jmmc.oiexplorer.core.model;
 
-import fr.jmmc.oitools.OIFitsConstants;
+import fr.jmmc.jmcs.jaxb.JAXBFactory;
+import fr.jmmc.jmcs.jaxb.XmlBindException;
+import fr.jmmc.jmcs.util.FileUtils;
+import fr.jmmc.oiexplorer.core.model.plot.Axis;
+import fr.jmmc.oiexplorer.core.model.plot.PlotDefinition;
+import fr.jmmc.oiexplorer.core.model.plot.PlotDefinitions;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.UnmarshalException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -16,6 +28,9 @@ import java.util.Map;
  */
 public final class PlotDefinitionFactory {
 
+    /** Logger */
+    private static final Logger logger = LoggerFactory.getLogger(PlotDefinitionFactory.class.getName());
+    /** Factory instance */
     private final static PlotDefinitionFactory instance = new PlotDefinitionFactory();
     /** plot (Vis2 - T3) vs spatial freq */
     public final static String PLOT_VIS2DATA_T3PHI_SPATIAL_FREQ = "VIS2DATA_T3PHI/SPATIAL_FREQ";
@@ -25,54 +40,122 @@ public final class PlotDefinitionFactory {
     public final static String PLOT_VIS2DATA_T3PHI_MJD = "VIS2DATA_T3PHI/MJD";
     /** default plot */
     public final static String PLOT_DEFAULT = PLOT_VIS2DATA_T3PHI_SPATIAL_FREQ;
-
-    public static PlotDefinitionFactory getInstance() {
-        return instance;
-    }
-    /* members */
+    /** JAXB Factory */
+    private static JAXBFactory jf;
+    /** Presets Filename */
+    private final static String PRESETS_FILENAME = "fr/jmmc/oiexplorer/core/model/plotDefinitionPresets.xml";
     /** default plot definitions */
     private final Map<String, PlotDefinition> defaults = new LinkedHashMap<String, PlotDefinition>();
 
+    /** Return factory singleton instance */
+    public static PlotDefinitionFactory getInstance() {
+        return instance;
+    }
+
+    /**
+     * Private constructor for singleton pattern.
+     */
     private PlotDefinitionFactory() {
         initializeDefaults();
     }
 
+    /**
+     * Initialize default presets extracted from preset file.
+     */
     private void initializeDefaults() {
-        PlotDefinition plotDef;
 
-        plotDef = new PlotDefinition();
-        plotDef.setxAxis(OIFitsConstants.COLUMN_SPATIAL_FREQ);
-        plotDef.setIncludeZeroOnXAxis(true);
-        plotDef.setxAxisScalingFactor(1e-6d);
-        plotDef.setxAxisUnit("M\u03BB");
-        plotDef.setyAxes(Arrays.asList(new String[]{OIFitsConstants.COLUMN_VIS2DATA, OIFitsConstants.COLUMN_T3PHI}));
-        defaults.put(PLOT_VIS2DATA_T3PHI_SPATIAL_FREQ, plotDef);
+        logger.warn("Loading presets from : {}", PRESETS_FILENAME);
 
-        plotDef = new PlotDefinition();
-        plotDef.setxAxis(OIFitsConstants.COLUMN_EFF_WAVE);
-        plotDef.setxAxisScalingFactor(1e6d);
-        plotDef.setxAxisUnit("micrometer");
-        plotDef.setyAxes(Arrays.asList(new String[]{OIFitsConstants.COLUMN_VIS2DATA, OIFitsConstants.COLUMN_T3PHI}));
-        defaults.put(PLOT_VIS2DATA_T3PHI_EFF_WAVE, plotDef);
-
-        plotDef = new PlotDefinition();
-        plotDef.setxAxis(OIFitsConstants.COLUMN_MJD);
-        plotDef.setyAxes(Arrays.asList(new String[]{OIFitsConstants.COLUMN_VIS2DATA, OIFitsConstants.COLUMN_T3PHI}));
-        defaults.put(PLOT_VIS2DATA_T3PHI_MJD, plotDef);
-
-        if (false) {
-            plotDef = new PlotDefinition();
-            plotDef.setxAxis(OIFitsConstants.COLUMN_MJD);
-            plotDef.setyAxes(Arrays.asList(new String[]{OIFitsConstants.COLUMN_UCOORD, OIFitsConstants.COLUMN_VCOORD}));
-            defaults.put(PLOT_DEFAULT, plotDef);
+        PlotDefinitions presets;
+        try {
+            presets = (PlotDefinitions) loadObject(FileUtils.getResource(PRESETS_FILENAME));;
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can't load default preset file from " + PRESETS_FILENAME, ioe);
+        } catch (IllegalStateException ise) {
+            throw new IllegalStateException("Can't load default preset file from " + PRESETS_FILENAME, ise);
+        } catch (XmlBindException xbe) {
+            throw new IllegalStateException("Can't load default preset file from " + PRESETS_FILENAME, xbe);
         }
+
+        /* Store defaults computing names (actually, as described in constants ) */
+        for (PlotDefinition plotDefinition : presets.getPlotDefinitions()) {
+            StringBuilder sb = new StringBuilder();
+            for (Axis yAxis : plotDefinition.getYAxes()) {
+                sb.append(yAxis.getName()).append("_");
+            }
+            sb.replace(sb.length() - 1, sb.length(), "/");
+            sb.append(plotDefinition.getXAxis().getName());
+
+            defaults.put(sb.toString(), plotDefinition);
+        }
+
     }
 
+    
+    /** Get default presets.
+     * @return List of default plotDefinitions
+     */
     public List<String> getDefaultList() {
         return new ArrayList<String>(defaults.keySet());
     }
 
+    /** Get the default preset given to its name. 
+     @return PlotDefinition associated to given name.*/
     public PlotDefinition getDefault(final String key) {
         return defaults.get(key);
+    }
+    
+    /**
+     * Load on object from url.
+     * @param inputUrl File to load
+     * @return unmarshalled object
+     *
+     * @throws IOException if an I/O exception occured
+     * @throws IllegalStateException if an unexpected exception occured
+     * @throws XmlBindException if a JAXBException was caught while creating an unmarshaller
+     */
+    private Object loadObject(URL inputUrl) throws IOException, IllegalStateException, XmlBindException {
+        Object result = null;
+
+        logger.warn("Load object from url : {}", inputUrl);
+
+        try {
+            result = getJAXBFactory().createUnMarshaller().unmarshal(new BufferedInputStream(inputUrl.openStream()));
+        } catch (JAXBException ex) {
+            handleException("Loading object from " + inputUrl, ex);
+        }
+
+        return result;
+    }
+
+    /**
+     * Handle JAXB Exception to extract IO Exception or unexpected exceptions
+     * @param message message
+     * @param je jaxb exception
+     * 
+     * @throws IllegalStateException if an unexpected exception occured
+     * @throws IOException if an I/O exception occured
+     */
+    protected static void handleException(final String message, final JAXBException je) throws IllegalStateException, IOException {
+        final Throwable cause = je.getCause();
+        if (cause != null) {
+            if (cause instanceof IOException) {
+                throw (IOException) cause;
+            }
+        }
+        if (je instanceof UnmarshalException) {
+            throw new IllegalArgumentException("The loaded file does not correspond to a valid file", je);
+        }
+        throw new IllegalStateException(message, je);
+    }
+
+    /** Return the jaxbfactory to manage PlotDefinition.xsd material.
+     @return the JAXBFactory for PlotDefinition related objects
+     */
+    private JAXBFactory getJAXBFactory() {
+        if (jf == null) {
+            jf = JAXBFactory.getInstance(PlotDefinition.class.getPackage().getName());
+        }
+        return jf;
     }
 }
