3055d5f59bda803d5a860ee08b83dfc5a959c3db
bourgesl <bourges.laurent@gmail.com>
2012-09-11 14:02:11 +0000

    moved code into jmcs ObjectUtils implements PublicCloneable and ToStringable

--- src/fr/jmmc/oiexplorer/core/model/OIBase.java
+++ src/fr/jmmc/oiexplorer/core/model/OIBase.java
@@ -3,8 +3,9 @@
  ******************************************************************************/
 package fr.jmmc.oiexplorer.core.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import fr.jmmc.jmcs.util.ObjectUtils;
+import fr.jmmc.jmcs.util.PublicCloneable;
+import fr.jmmc.jmcs.util.ToStringable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,16 +13,37 @@ import org.slf4j.LoggerFactory;
  * This is a base class for all generated classes in model.oi (Optical Interferometry Data Model)
  * @author bourgesl
  */
-public class OIBase implements Cloneable {
+public class OIBase implements PublicCloneable, ToStringable {
 
     /** Class logger for model classes */
     protected static final Logger logger = LoggerFactory.getLogger(OIBase.class.getName());
+    /** flag to use full verbosity in toString() implementation */
+    public static final boolean TO_STRING_VERBOSITY = true;
+
+    /* member */
+    /** instance version to track effective changes */
+    private int version = 0;
 
     /**
      * Public Constructor
      */
     public OIBase() {
         super();
+    }
+
+    /**
+     * Return the instance version
+     * @return instance version
+     */
+    public final int getVersion() {
+        return version;
+    }
+
+    /**
+     * PROTECTED: Increment the version
+     */
+    final void incVersion() {
+        this.version++;
     }
 
     /**
@@ -55,50 +77,29 @@ public class OIBase implements Cloneable {
     }
 
     /**
-     * Return a deep "copy" of the list of objects (recursive call to clone() on each object instance)
-     * @param <K> OIBase child class
-     * @param list list of objects to clone
-     * @return deep "copy" of the list
+     * toString() implementation wrapper to get complete information
+     * Note: prefer using @see #toString(java.lang.StringBuilder) instead
+     * @return string representation
      */
-    @SuppressWarnings("unchecked")
-    public static <K extends OIBase> List<K> deepCopyList(final List<K> list) {
-        if (list != null) {
-            final List<K> newList = new ArrayList<K>(list.size());
-            for (K o : list) {
-                newList.add((K) o.clone());
-            }
-            return newList;
-        }
-        return null;
+    @Override
+    public final String toString() {
+        final StringBuilder sb = new StringBuilder(256);
+        toString(sb, TO_STRING_VERBOSITY);
+        return sb.toString();
     }
 
     /**
-     * Return a simple "copy" of the list of objects without cloning each object instance
-     * @param <K> OIBase child class
-     * @param list list of objects to clone
-     * @return deep "copy" of the list
+     * toString() implementation using string builder
+     * 
+     * Note: to be overriden in child classes to append their fields
+     * 
+     * @param sb string builder to append to
+     * @param full true to get complete information; false to get main information (shorter)
      */
-    @SuppressWarnings("unchecked")
-    public static <K> List<K> copyList(final List<K> list) {
-        if (list != null) {
-            final List<K> newList = new ArrayList<K>(list.size());
-            for (K o : list) {
-                newList.add(o);
-            }
-            return newList;
+    public void toString(final StringBuilder sb, final boolean full) {
+        ObjectUtils.getObjectInfo(sb, this);
+        if (version != 0) {
+            sb.append("[version=").append(version).append(']');
         }
-        return null;
-    }
-
-    /**
-     * Utility method for <code>equals()</code> methods.
-     *
-     * @param o1 one object
-     * @param o2 another object
-     *
-     * @return <code>true</code> if they're both <code>null</code> or both equal
-     */
-    public static boolean areEquals(final Object o1, final Object o2) {
-        return (o1 == o2) || (o1 != null && o1.equals(o2));
     }
 }
