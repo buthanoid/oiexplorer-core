2e77e6a77dd90f2f2d2f21bf837d3e4187d59b43
bourgesl <bourges.laurent@gmail.com>
2012-09-12 14:25:50 +0000

    extends Disposable to force listener to implement dispose() to unregister from event notifiers

--- src/fr/jmmc/oiexplorer/core/model/OIFitsCollectionManagerEventListener.java
+++ src/fr/jmmc/oiexplorer/core/model/OIFitsCollectionManagerEventListener.java
@@ -3,13 +3,15 @@
  ******************************************************************************/
 package fr.jmmc.oiexplorer.core.model;
 
+import fr.jmmc.jmcs.gui.component.Disposable;
 import fr.jmmc.oiexplorer.core.model.event.GenericEventListener;
 
 /**
  * This interface define the methods to be implemented by OIFitsCollectionManagerEvent listener implementations
+ * and force listener to be disposable to unregister them
  * 
  * @author bourgesl
  */
 public interface OIFitsCollectionManagerEventListener
-        extends GenericEventListener<OIFitsCollectionManagerEvent, OIFitsCollectionManagerEventType, Object> {
+        extends Disposable, GenericEventListener<OIFitsCollectionManagerEvent, OIFitsCollectionManagerEventType, Object> {
 }
