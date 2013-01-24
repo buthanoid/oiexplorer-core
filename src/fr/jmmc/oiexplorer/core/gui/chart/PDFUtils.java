3de0d4cf216b2d55c51620a72efc1f415a14e97a
sly74fr <slymacdev@free.fr>
2013-01-24 16:29:32 +0000

    Moved all ApplicationDataModel related stuff out of App to the new ApplicationDescription singleton.

--- src/fr/jmmc/oiexplorer/core/gui/chart/PDFUtils.java
+++ src/fr/jmmc/oiexplorer/core/gui/chart/PDFUtils.java
@@ -12,6 +12,7 @@ import com.lowagie.text.pdf.PdfContentByte;
 import com.lowagie.text.pdf.PdfTemplate;
 import com.lowagie.text.pdf.PdfWriter;
 import fr.jmmc.jmcs.App;
+import fr.jmmc.jmcs.data.ApplicationDescription;
 import fr.jmmc.jmcs.util.FileUtils;
 import fr.jmmc.oiexplorer.core.gui.chart.PDFOptions.Orientation;
 import java.awt.Graphics2D;
@@ -174,7 +175,7 @@ public final class PDFUtils {
      * @param document pdf document
      */
     private static void definePDFProperties(final Document document) {
-        document.addCreator(App.getSharedApplicationDataModel().getProgramName() + " v" + App.getSharedApplicationDataModel().getProgramVersion());
+        document.addCreator(ApplicationDescription.getInstance().getProgramName() + " v" + ApplicationDescription.getInstance().getProgramVersion());
     }
 
     /**
