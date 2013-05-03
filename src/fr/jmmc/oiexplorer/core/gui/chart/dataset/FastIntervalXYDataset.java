abeedcfffe59eaa6ed1c5c167c6c46763cd4f096
bourgesl <bourges.laurent@gmail.com>
2013-05-03 15:32:30 +0000

    removed dead code

--- src/fr/jmmc/oiexplorer/core/gui/chart/dataset/FastIntervalXYDataset.java
+++ src/fr/jmmc/oiexplorer/core/gui/chart/dataset/FastIntervalXYDataset.java
@@ -121,17 +121,6 @@ public final class FastIntervalXYDataset<K extends Comparable<V>, V> extends Abs
         final Integer seriesIndex = this.seriesKeys.get(seriesKey);
 
         return (seriesIndex != null) ? seriesIndex.intValue() : -1;
-
-        // Bad performance code:
-        /*
-         int seriesCount = getSeriesCount();
-         for (int s = 0; s < seriesCount; s++) {
-         if (getSeriesKey(s).equals(seriesKey)) {
-         return s;
-         }
-         }
-         return -1;
-         */
     }
 
     /**
