358ec136a639b85d9568993725467e2ee8b3b5db
bourgesl <bourges.laurent@gmail.com>
2016-09-27 13:14:09 +0000

    fixed precision when comparing wavelength (1/2 channel width)

--- src/main/java/fr/jmmc/oiexplorer/core/model/InstrumentModeManager.java
+++ src/main/java/fr/jmmc/oiexplorer/core/model/InstrumentModeManager.java
@@ -13,6 +13,8 @@ import java.util.List;
  * @author bourgesl
  */
 public final class InstrumentModeManager extends AbstractMapper<InstrumentMode> {
+    /** smallest precision on wavelength */
+    public final static float LAMBDA_PREC = 1e-10f;
 
     /** Singleton pattern */
     private final static InstrumentModeManager instance = new InstrumentModeManager();
@@ -42,16 +44,23 @@ public final class InstrumentModeManager extends AbstractMapper<InstrumentMode> 
         if (NumberUtils.compare(src.getNbChannels(), other.getNbChannels()) != 0) {
             return false;
         }
+
+        // precision = 1/2 channel width ie min(eff_band)/2
+        float prec = 0.5f * Math.min(src.getBandMin(), other.getBandMin());
         
+        if (Float.isNaN(prec) || prec < LAMBDA_PREC) {
+            prec = LAMBDA_PREC;
+        }
+
         // precision = 1e-10 ie 3 digits in nm:
-        if (!NumberUtils.equals(src.getLambdaMin(), other.getLambdaMin(), 1e-10f)) {
+        if (!NumberUtils.equals(src.getLambdaMin(), other.getLambdaMin(), prec)) {
             return false;
         }
-        if (!NumberUtils.equals(src.getLambdaMax(), other.getLambdaMax(), 1e-10f)) {
+        if (!NumberUtils.equals(src.getLambdaMax(), other.getLambdaMax(), prec)) {
             return false;
         }
-        // precision = +/- 0.5:
-        return (NumberUtils.equals(src.getLambdaMax(), other.getLambdaMax(), 5e-1f));
+        // precision = +/- 1:
+        return (NumberUtils.equals(src.getLambdaMax(), other.getLambdaMax(), 1.0));
     }
 
     @Override
