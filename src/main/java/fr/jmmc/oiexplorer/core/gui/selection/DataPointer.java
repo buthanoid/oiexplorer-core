cc4d85c010cb9f137c439c7f1c2bac94a8c2d2aa
bourgesl <bourges.laurent@gmail.com>
2016-10-19 16:02:10 +0000

    added complementary info (target, hour angle, pos angle, spatial freq ...)

--- src/main/java/fr/jmmc/oiexplorer/core/gui/selection/DataPointer.java
+++ src/main/java/fr/jmmc/oiexplorer/core/gui/selection/DataPointer.java
@@ -5,6 +5,7 @@ package fr.jmmc.oiexplorer.core.gui.selection;
 
 import fr.jmmc.jmcs.util.NumberUtils;
 import fr.jmmc.oitools.model.OIData;
+import fr.jmmc.oitools.model.OITarget;
 
 /**
  *
@@ -15,7 +16,7 @@ public final class DataPointer {
     public final static int UNDEFINED = -1;
 
     /* member */
- /* data table */
+    /** data table */
     private final OIData oiData;
     /* row index in the data table */
     private final int row;
@@ -38,6 +39,39 @@ public final class DataPointer {
 
     public int getCol() {
         return col;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + (this.oiData != null ? this.oiData.hashCode() : 0);
+        hash = 97 * hash + this.row;
+        hash = 97 * hash + this.col;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final DataPointer other = (DataPointer) obj;
+        if (this.row != other.getRow()) {
+            return false;
+        }
+        if (this.col != other.getCol()) {
+            return false;
+        }
+        if (this.oiData != other.getOiData() && (this.oiData == null || !this.oiData.equals(other.getOiData()))) {
+            return false;
+        }
+        return true;
     }
 
     @Override
@@ -87,4 +121,53 @@ public final class DataPointer {
         return oiData.getOIFitsFile().getName();
     }
 
+// Fast access to computed values:    
+    public String getTarget() {
+        if (checkRow()) {
+            final short targetId = oiData.getTargetId()[row];
+
+            final OITarget oiTarget = oiData.getOiTarget();
+
+            if (oiTarget != null) {
+                final Integer rowTarget = oiTarget.getRowIndex(Short.valueOf(targetId)); // requires previously OIFits Analyzer call()
+                if (rowTarget != null) {
+                    return oiTarget.getTarget()[rowTarget.intValue()];
+                }
+            }
+
+        }
+        return "";
+    }
+
+    public double getSpatialFreq() {
+        if (checkRow() && checkCol()) {
+            return oiData.getSpatialFreq()[row][col];
+        }
+        return Double.NaN;
+    }
+
+    public double getRadius() {
+        if (checkRow()) {
+            return oiData.getRadius()[row];
+        }
+        return Double.NaN;
+    }
+
+    public double getPosAngle() {
+        if (checkRow()) {
+            return oiData.getPosAngle()[row];
+        }
+        return Double.NaN;
+    }
+
+    public double getHourAngle() {
+        if (checkRow()) {
+            return oiData.getHourAngle()[row];
+        }
+        return Double.NaN;
+    }
+
+    public boolean isSameCol(final DataPointer other) {
+        return (oiData == other.getOiData()) && (col == other.getCol());
+    }
 }
