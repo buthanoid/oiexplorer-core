50e1e87b3c99be8de31e09d60213927c8b634bb1
bourgesl <bourges.laurent@gmail.com>
2016-10-19 16:01:43 +0000

    added axis info

--- src/main/java/fr/jmmc/oiexplorer/core/gui/selection/DataPoint.java
+++ src/main/java/fr/jmmc/oiexplorer/core/gui/selection/DataPoint.java
@@ -3,19 +3,30 @@
  ******************************************************************************/
 package fr.jmmc.oiexplorer.core.gui.selection;
 
+import fr.jmmc.oiexplorer.core.gui.AxisInfo;
+
 /**
  *
  * @author bourgesl
  */
 public class DataPoint {
-    
-    public static final DataPoint UNDEFINED = new DataPoint(Double.NaN, Double.NaN);
+
+    public static final DataPoint UNDEFINED = new DataPoint(null, null, Double.NaN, Double.NaN);
 
     /* members */
+    /** x axis information */
+    private final AxisInfo xAxisInfo;
+    /** y axis information */
+    private final AxisInfo yAxisInfo;
+    /** x coordinate */
     private final double x;
+    /** y coordinate */
     private final double y;
 
-    public DataPoint(final double x, final double y) {
+    public DataPoint(final AxisInfo xAxisInfo, final AxisInfo yAxisInfo,
+                     final double x, final double y) {
+        this.xAxisInfo = xAxisInfo;
+        this.yAxisInfo = yAxisInfo;
         this.x = x;
         this.y = y;
     }
@@ -26,6 +37,14 @@ public class DataPoint {
 
     public final double getY() {
         return y;
+    }
+
+    public final AxisInfo getxAxisInfo() {
+        return xAxisInfo;
+    }
+
+    public final AxisInfo getyAxisInfo() {
+        return yAxisInfo;
     }
 
     @Override
--- src/main/java/fr/jmmc/oiexplorer/core/gui/selection/DataPointInfo.java
+++ src/main/java/fr/jmmc/oiexplorer/core/gui/selection/DataPointInfo.java
@@ -3,6 +3,8 @@
  ******************************************************************************/
 package fr.jmmc.oiexplorer.core.gui.selection;
 
+import fr.jmmc.oiexplorer.core.gui.AxisInfo;
+
 /**
  *
  * @author bourgesl
@@ -11,8 +13,10 @@ public final class DataPointInfo extends DataPoint {
 
     private final DataPointer ptr;
 
-    public DataPointInfo(final double x, final double y, final DataPointer ptr) {
-        super(x, y);
+    public DataPointInfo(final AxisInfo xAxisInfo, final AxisInfo yAxisInfo,
+                         final double x, final double y,
+                         final DataPointer ptr) {
+        super(xAxisInfo, yAxisInfo, x, y);
         this.ptr = ptr;
     }
 
